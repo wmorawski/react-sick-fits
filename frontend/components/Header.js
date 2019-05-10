447af7760656e5df5f94cb528ea1538c476d05d7
Wiktor Morawski <morwiko@gmail.com>
2019-05-10 15:41:50 +0200

    Initial commit

---
+++ frontend/components/Header.js
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Sick Fists</a>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+)
+export default Header
