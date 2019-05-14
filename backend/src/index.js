017bb8ed0006d56f982e6810f62b14035171069e
Wiktor Morawski <morwiko@gmail.com>
2019-05-14 12:33:47 +0200

    Prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,12 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db')
+
+const server = createServer();
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+});
